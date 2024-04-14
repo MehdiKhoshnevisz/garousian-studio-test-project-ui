c23c22533ded3aa51f0988335fe3a5e46f525e0a
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2024-04-14 16:28:09 +0330

    Fix contact us section styles

--- src/components/ContactUsSection/ContactUsSection.styled.js
+++ src/components/ContactUsSection/ContactUsSection.styled.js
@@ -5,26 +5,52 @@ import {
   LogoWrapper,
   LogoCaption,
   MetaSection,
-  MetaSectionTitle,
-  MetaSectionCaption,
   CallToActionsWrapper,
+  MetaSectionTitle as TopMetaSectionTitle,
+  MetaSectionCaption as TopMetaSectionCaption,
 } from "../TopSection/TopSection.styled";
 import { SectionWrapper } from "@/components/Base/SectionWrapper";
 
 import ContactUsBackgroundImage from "/public/images/contact-us-bg.png";
 
 const Wrapper = styled(SectionWrapper)({
-  minHeight: "520px",
   textAlign: "center",
+  position: "relative",
+  paddingTop: "140.5px",
   backgroundSize: "cover",
+  paddingBottom: "140.5px",
   backgroundPosition: "center",
   backgroundRepeat: "no-repeat",
-  backgroundBlendMode: "multiply",
-  backgroundColor: "rgba(0, 0, 0, 0.6)",
   backgroundImage: `url(${ContactUsBackgroundImage.src})`,
 
+  "&:before": {
+    opacity: 0.6,
+    content: '""',
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    backgroundColor: "#000000",
+  },
+
   "@media(max-width: 768px)": {
-    minHeight: "360px",
+    paddingTop: "67px",
+    paddingBottom: "67px",
+  },
+});
+
+const MetaSectionTitle = styled(TopMetaSectionTitle)({
+  fontSize: "1.75rem",
+
+  "@media(max-width: 768px)": {
+    fontSize: "1rem",
+  },
+});
+
+const MetaSectionCaption = styled(TopMetaSectionCaption)({
+  fontSize: "1.125rem",
+
+  "@media(max-width: 768px)": {
+    fontSize: "0.875rem",
   },
 });
 
